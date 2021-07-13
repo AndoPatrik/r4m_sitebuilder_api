9e122f2baebf9b0ee7cd34e6801c6c98e28a3d10
AndoPatrik <32640906+AndoPatrik@users.noreply.github.com>
2021-07-13 20:23:46 +0200

    Restructure + getblockbyid

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
-const express = require('express')
-const cors = require('cors')
-const {pool} = require('./config')
+const express = require("express");
+const cors = require("cors");
+const { pool } = require("./config");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-const getBlocks = (request, response) => {
-  pool.query('SELECT * FROM blocks', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
+// START OF ACTIONS
+app.get("/api/blocks", async (request, response) => {
+  try {
+    const blocks = await pool.query("SELECT * FROM blocks");
+    response.json(blocks.rows);
+  } catch (error) {
+    throw error;
+  }
+});
 
-const addBlock = (request, response) => {
-  const {type, md, twig, yaml, html} = request.body
+app.get("/api/blocks/:id", async (request, response) => {
+  const { id } = request.params;
+  try {
+    const block = await pool.query("SELECT * FROM blocks where id=$1", [id]);
+    if (block.rows[0]) response.json(block.rows[0]);
+    response.status(404).json({ message: "Block does not exist" });
+  } catch (error) {
+    throw error;
+  }
+});
 
-  pool.query(
-    'INSERT INTO blocks (type, md, twig, yaml, html) VALUES ($1, $2, $3, $4, $5)',
-    [type, md, twig, yaml, html],
-    (error) => {
-      if (error) {
-        throw error
-      }
-      response.status(201).json({status: 'success', message: 'Block added.'})
-    },
-  )
-}
-
-app
-  .route('/blocks')
-  .get(getBlocks)
-  .post(addBlock)
+app.post("/api/blocks/", async (request, response) => {
+  try {
+    const { type, md, twig, yaml, html } = request.body;
+    await pool.query(
+      "INSERT INTO blocks (type, md, twig, yaml, html) VALUES ($1, $2, $3, $4, $5)",
+      [type, md, twig, yaml, html]
+    );
+    response.status(201).json({ status: "success", message: "Block added." });
+  } catch (error) {
+    throw error;
+  }
+});
+// END OF ACTIONS
 
 app.listen(process.env.PORT || 3002, () => {
-  console.log(`Server listening`)
-})
+  console.log(`Server listening`);
+});
