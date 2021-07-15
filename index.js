bda32a6320db66a6e904c6fe4d91a7b0e26d8484
AndoPatrik <32640906+AndoPatrik@users.noreply.github.com>
2021-07-15 23:55:33 +0200

    Add cors *

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.options("*", cors());
 
 // START OF ACTIONS
 app.get("/api/blocks", async (request, response) => {
